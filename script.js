7a02a28d282adaedc8b5e976eb77646cfad0f446
jakmis <jakmis007gmail.com>
2021-01-18 20:36:44 +0100

    První commit

---
+++ script.js
@@ -0,0 +1,163 @@
+/*Konstanty*/
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+/*Plátno*/
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+/*Player*/
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    /*Nakreslení Player*/
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+/*Projectile*/
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    };
+    /*Nakreslení Projectile*/
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    };
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    };
+};
+/*Enemy */
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    };
+    /*Nakreslení Projectile*/
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    };
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    };
+};
+/*Posunutí na Player střed*/
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 15, 'white');
+
+const projectiles = [];
+const enemies = [];
+
+function spawnEnemies(){
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
+        let x;
+        let y;
+        if (Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        }
+        else{
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        };
+        /*Generátor náhodné barvy nepřítel*/
+        const color = `hsl( ${Math.random() * 360}, 50%, 50%)`;
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        };
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+        }, 1000);
+}
+
+let animationId
+
+/*Opakující se funkce*/
+function animate() {
+    animationId = requestAnimationFrame(animate);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach(function (projectile, index) {
+            projectile.update();
+            /*Odstránění projectiles z obrazovky*/
+            if (projectile.x + projectile.radius < 0 || 
+                projectile.x - projectile.radius > canvas.width ||
+                projectile.y + projectile.radius < 0 ||
+                projectile.y - projectile.radius > canvas.height) {
+                setTimeout(() => {
+                    projectiles.splice(index, 1);
+                }, 0);
+            }
+        })
+
+    /*Vytváření nepřátel*/
+    enemies.forEach((enemy, index) => {
+        enemy.update();
+
+        /*Detekce kolize hráče*/
+        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+        /*Ukončení hry, zastavení funkce animate*/
+        if (distance - enemy.radius - player.radius < 1) {
+            cancelAnimationFrame(animationId);
+        }
+
+        /*Vzdálenost nepřátel od střely*/
+        projectiles.forEach((projectile, projectileIndex) => {
+            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+            /*Detekce kolize střely a zničení nepřátel*/
+            if (distance - enemy.radius - projectile.radius < 1) {
+
+                /*Plynulejší zničení nepřátel - SetTimeout(0)*/
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    projectiles.splice(projectileIndex, 1);
+                }, 0);
+            }
+        });
+    })
+};
+
+
+window.addEventListener('click', function(event){
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
+    }
+    projectiles.push(new Projectile(
+        canvas.width / 2, canvas.height / 2, 5, 'white', velocity
+    )
+)
+})
+
+animate();
+spawnEnemies();
